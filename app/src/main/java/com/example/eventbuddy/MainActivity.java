a71cac239096fe935c078c8a3c25106bf5dd4515
elifBalci <ebalci9949@gmail.com>
2019-05-11 18:37:38 +0300

    event adder is added.

--- app/src/main/java/com/example/eventbuddy/MainActivity.java
+++ app/src/main/java/com/example/eventbuddy/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.eventbuddy;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        private String displayName;
+        private ImageButton sendButton;
+        private ListView chatListView;
+        private Button settingButton;
+
+
+        private DatabaseReference databaseReference;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            setupDisplayName();
+
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+
+            sendButton = findViewById(R.id.addEventButton);
+            chatListView = findViewById(R.id.chat_list_view);
+
+        }
+        public void addEventHandler(View view) {
+            Intent intent = new Intent(MainActivity.this, AddEventActivity.class);
+            finish();
+            startActivity(intent);
+        }
+
+        private void setupDisplayName() {
+            SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+            displayName = sharedPref.getString("username", null);
+            if (displayName == null) {
+                displayName = "anonymous";
+            }
+        }
+
+
+
 }
+
